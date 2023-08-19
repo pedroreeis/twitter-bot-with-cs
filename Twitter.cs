76c8c2f784454285d91ccbe41ff3bfe5dd9f6530
Pedro de Andrade Reis <61993076+pedroreeis@users.noreply.github.com>
2023-08-19 16:31:25 -0300

    Remove credentials

--- Twitter.cs
+++ Twitter.cs
@@ -17,10 +17,10 @@ namespace TwitterBot
         public static void Main(String[] args)
         {
             /// Cria as variaveis onde se encontra os dados de autenticação
-            string TWITTER_OAUTH2_CLIENT_ID = "L2EO6RB2FA9pvIq6eSEDgxPlw";
-            string TWITTER_OAUTH2_CLIENT_SECRET = "5XqMGqKOKvITbbUQkE3f0Xue9KATTg0tDcv4bG7DW12RhaobAv";
-            string TWITTER_OAUTH2_ACCESS_TOKEN = "943604245911228417-1SrXLbcpsMm3ABmgdi1BDZKGNWcyu84";
-            string TWITTER_OAUTH2_REFRESH_TOKEN = "QXvGfHIB5szZAiJMluXhMxp4yUqm7s8eBttk3KoUe90eA";
+            string TWITTER_OAUTH2_CLIENT_ID = "";
+            string TWITTER_OAUTH2_CLIENT_SECRET = "";
+            string TWITTER_OAUTH2_ACCESS_TOKEN = "";
+            string TWITTER_OAUTH2_REFRESH_TOKEN = "";
 
             // Construtor do client
             var clientTwitter = new TwitterClient(TWITTER_OAUTH2_CLIENT_ID, TWITTER_OAUTH2_CLIENT_SECRET, TWITTER_OAUTH2_ACCESS_TOKEN, TWITTER_OAUTH2_REFRESH_TOKEN);
