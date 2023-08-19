2c98cffb62d6f340943c4ab52e0126fb1c95822b
Pedro de Andrade Reis <61993076+pedroreeis@users.noreply.github.com>
2023-08-19 16:30:12 -0300

    Update TweetCommand.cs

--- Commands/TweetCommand.cs
+++ Commands/TweetCommand.cs
@@ -1,12 +1,33 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tweetinvi;
 
 namespace TwitterBot.Commands
 {
-    internal class TweetCommand
+    public class TweetCommand
     {
+        private TwitterClient client;
+
+        public TweetCommand(TwitterClient twitterClient) {
+            this.client = twitterClient;
+        }
+
+        public async Task<Tweetinvi.Models.ITweet> Send(string message)
+        {
+            var tweetForSend = await client.Tweets.PublishTweetAsync(message);
+            var tweetPublished = await client.Tweets.GetTweetAsync(tweetForSend.Id);
+
+            return tweetPublished;
+        }
+
+        public async Task<Tweetinvi.Models.V2.TweetV2Response> Get(int  id)
+        {
+            var tweetSearch = await client.TweetsV2.GetTweetAsync(id);
+
+            return tweetSearch;
+        }
     }
 }
